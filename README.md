3906d01191d59d4db3f9453cbe7f4d41e5fe8473
Heiko Klausing <h.klausing@gmx.de>
2016-11-05 17:00:54 +0100

    README.md added; install.sh got some functions for better script flow

---
+++ README.md
@@ -0,0 +1,35 @@
+dotfiles
+========
+
+dotfile is used to keep all system relevant configuration file at one
+location. The reason for this:
+
+* This will allow to cover the files in an easy way under
+version control.
+* On the other site it makes sharing of setups
+with other system very convenient.
+
+This repository can be find <http://github.com/hklausing/dotfiles.git>.
+
+
+Requirements
+------------
+
+* Used shell is bash
+* Test system is Manjaro
+
+
+Installation
+------------
+
+Follow next commands to update the system:
+
+~~~{.bash}
+$ cd ~
+$ git clone git://github.com/hklausing/dotfiles.git .dotfiles
+$ cd .dotfiles
+$ ./install.sh
+~~~~~~~~~~
+
+
+
--- install.sh
+++ install.sh
@@ -1,32 +1,45 @@
 #!/usr/bin/env bash
 ###########################################################################
-# .make.sh
+# File      : install.sh
+# Author    : Heiko Klausing
+# Creation  : 2016-11-05
+#
 # This script creates symlinks from the home directory to any
 # desired dotfiles in ~/.dotfiles. Existing files will be stored
 # in the directory ~/.dotfiles/.original.
 # The directory ~/.dotfiles is version controlled.
+#
 ###########################################################################
 
-echo "Install dotfiles:"
+
+# Name of the directory of dotfile locations.
+g_trgdir=~/.dotfiles
+# Name of storage location relative to dotfile directory
+g_store=".original"
 
 
-# Name of the director of dotfile locations.
-trgdir=~/.dotfiles
-storedir="${trgdir}/.original"
+
 
 
 #==========================================================================
-# This function does some actions:
-# - Copies existing original files to a storage location.
-# - Creates a link to the given parameter
+# Get the current processed source path.
 # Param1:   source directory, relative to ditfile directory
-# Param2:   target directory, relative to home directory or absolute path
-# Param3:   file name
+# Param2:   file name
 #==========================================================================
-function setLink()
+function getSourceFile()
 {
-    local src="${trgdir}/$1/$3"
-    local trg="~/$2/$3"
+    echo "${g_trgdir}/$1/$2"
+}
+
+
+#==========================================================================
+# Get the current processed target path.
+# Param1:   target directory, relative to home directory or absolute path
+# Param2:   file name
+#==========================================================================
+function getTargetFile()
+{
+    local trg="~/$1/$2"
 
     if [[ $2 =~ ^/ ]]; then
         # target path is absolute
@@ -38,30 +51,66 @@ function setLink()
     # replace existing tilde '~'
     trg=$(eval echo "${trg}")
 
-    echo -n "create ${src} to ${trg}"
+    echo ${trg}
+}
 
-    # test if source file exists
-    if [[ ! -f ${src} ]]; then
-        echo
-        echo "ERROR: file '${src}' not found"
-        exit 1
-    fi
 
-    # create target directory
-    mkdir -p $(dirname ${trg})
+#==========================================================================
+# If the source file is not a link than copy the file to a storage 
+# location.
+# Param1:   source directory, relative to ditfile directory
+# Param2:   target directory, relative to home directory or absolute path
+# Param3:   file name
+#==========================================================================
+function saveOriginal()
+{
+    local trg="$1"
+    local trgdir="$2"
 
     # test if original file exists
     if [[ -f "${trg}" && ! -L "${trg}" ]]; then
+
+        local storedir="${g_trgdir}/${g_store}"
+
         echo
         echo "file '${trg}' not a symbolic link"
 
         # copy file
-        mkdir -p ${storedir}/$1/
-        cp ${trg} ${storedir}/$1/
+        mkdir -p ${storedir}/${trgdir}/
+        cp ${trg} ${storedir}/${trgdir}/
         rm ${trg}
-        echo "  ${trg} moved to '${storedir}/$1/'"
+        echo "  ${trg} moved to '${storedir}/${trgdir}/'"
         echo -n "  create link "
     fi
+}
+
+
+#==========================================================================
+# This function does some actions:
+# - Copies existing original files to a storage location.
+# - Creates a link to the given parameter
+# Param1:   source directory, relative to dotfile directory
+# Param2:   target directory, relative to home directory or absolute path
+# Param3:   file name
+#==========================================================================
+function setLink()
+{
+    local src=$(getSourceFile "$1" "$3")
+    local trg=$(getTargetFile "$2" "$3")
+
+    # test if source file exists
+    if [[ ! -f ${src} ]]; then
+        echo "ERROR: file '${src}' not found"
+        exit 1
+    fi
+
+    echo -n "create ${src} to ${trg}"
+
+    # create target directory
+    mkdir -p $(dirname ${trg})
+
+    # store existing source file if not a link
+    saveOriginal "${trg}" "$1"
 
     # make a link
     ln -sf ${src} ${trg}
@@ -77,6 +126,9 @@ function setLink()
 
 }
 
+
+
+echo "Install dotfiles:"
 
 
 ###########################################################################
